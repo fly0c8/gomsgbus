67ff48fe7d932456fe7ed4e756a439683c294daf
anar <arnold.angerer@skidata.com>
2019-10-04 13:40:22 +0200

    msghub

--- gomsgbus.go
+++ gomsgbus.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"nanomsg.org/go/mangos/v2"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/labstack/echo"
-	"nanomsg.org/go/mangos/v2"
 	"nanomsg.org/go/mangos/v2/protocol/bus"
 	_ "nanomsg.org/go/mangos/v2/transport/all"
 )
 
 var (
-	nodename     = os.Args[1]
-	cmdport      = os.Args[2]
-	myurl        = os.Args[3]
-	firstPeerIdx = 4
-	minArgLength = 5
+	nodename            = os.Args[1]
+	cmdport             = os.Args[2]
+	myurl               = os.Args[3]
+	firstPeerIdx        = 4
+	minArgLength        = 5
+	incomingBusChan     = make(chan string)
+	incomingHttpCmdChan = make(chan string)
 )
 
 func die(format string, v ...interface{}) {
@@ -33,33 +36,56 @@ func main() {
 	}
 
 	busSocket := busSetup()
-	go busHandler(busSocket)
+	go msgHub(busSocket)
+	go busReceiver(busSocket)
 
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello GoMsgBus!")
-	})
-	e.POST("/cmd", func(c echo.Context) error {
-		err := busSocket.Send([]byte(fmt.Sprintf("The time now is: %v", time.Now())))
-		if err != nil {
-			return c.String(http.StatusInternalServerError, fmt.Sprintf("%#v", err))
-		}
-		return c.String(http.StatusOK, "Kommando erfolgreich gepostet...")
-	})
-	e.HideBanner = true
+	e := httpSetup()
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":" + cmdport)))
 
 }
-func busHandler(busSocket mangos.Socket) {
+
+func busReceiver(busSocket mangos.Socket) {
 	// Start receiving...
 	for {
 		msg, err := busSocket.Recv()
 		if err != nil {
 			die("sock.Recv: %s", err.Error())
 		}
-		fmt.Printf("%s: RECEIVED \"%s\" FROM BUS\n", nodename, string(msg))
+		incomingBusChan <- string(msg)
 	}
 }
+
+func msgHub(busSocket mangos.Socket) {
+
+	for {
+		select {
+		case httpMsg := <-incomingHttpCmdChan:
+			err := busSocket.Send([]byte(httpMsg))
+			if err != nil {
+				fmt.Errorf("cannot send http cmd to bus: %#v", err)
+			}
+
+		case busMsg := <-incomingBusChan:
+			fmt.Printf("%s: RECEIVED \"%s\" FROM BUS\n", nodename, busMsg)
+		}
+	}
+
+}
+
+func httpSetup() *echo.Echo {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello GoMsgBus!")
+	})
+
+	e.POST("/cmd", func(c echo.Context) error {
+		incomingHttpCmdChan <- fmt.Sprintf("The time now is: %v", time.Now())
+		return c.String(http.StatusOK, "Kommando erfolgreich gepostet...")
+	})
+	e.HideBanner = true
+	return e
+}
+
 func busSetup() mangos.Socket {
 	// start bus listener
 	var busSocket mangos.Socket
@@ -73,9 +99,9 @@ func busSetup() mangos.Socket {
 		die("busSocket.Listen: %s", err.Error())
 	}
 
-	// wait for everyone to start listening
+	log.Println(nodename+" is waiting for peers....")
 	time.Sleep(time.Second)
-
+	log.Println("Continue....")
 	// connect to peers
 	for x := firstPeerIdx; x < len(os.Args); x++ {
 		if err = busSocket.Dial(os.Args[x]); err != nil {
